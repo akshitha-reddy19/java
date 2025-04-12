e25e3896688ba584d6e8e8a76c31fc0b564ad1ec
akshitha-reddy19 <akshithareddy1928@gmail.com>
2025-04-12 12:54:02 +0530

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,42 @@
+// class Peters{
+//     strength=1000
+
+//     constructor(){
+//         console.log("spec")
+//     }
+//     showStrength(){
+//         console.log(this.strength)
+//     }
+// }
+// class Peters1 extends Peters{
+//     constructor(){
+//         super()
+//     }
+//     bcd(){
+//         console.log(this.showStrength());
+//     }
+// }
+
+//  const obj=new Peters1()
+//  console.log(obj.bcd())
+// const oldArray = [74,45,56]
+// const newArray = [...oldArray,61,6]//spread operator
+// console.log(oldArray)
+// console.log(newArray)
+// function sortArgs(...args)
+// {
+//     return args.sort();
+
+// }
+// console.log(sortArgs(5,85,874,85,5))
+// function sortArgs(...args)
+// {
+//     return args.filter((e)=> e==67);
+
+// }
+// console.log(sortArgs(5,85,874,85,5))
+// const[a,b]=["MAnu","AKshu"]
+// console.log(a)
+const {name,age}={name:'AKSHU',age:16}
+console.log(name)
+console.log(age)
---
+++ script.js
@@ -0,0 +1,44 @@
+// var name="peters";//var is globally scoped
+// //let name="sty";
+// let number=10;// let is block scoped
+// console.log(name)
+// function abc(){
+//     {
+    
+//     var name="ece"
+//     let number=74
+//    }
+//    // name="ece"
+//     console.log(name)
+//     console.log(number)
+// }
+// abc()
+// function greet(){
+//     return "good afternoon"
+// }
+// const str=greet()
+// console.log(str)
+
+// const greet1=()=> "good evening[!]"
+
+// console.log(greet1())
+document.body.append("this is js cls")
+document.getElementById('w').innerText="HI PETERS"
+const heading = document.createElement('h2')
+heading.innerText="akshitha"
+document.bodyappend("this is js class")
+document.getElementById('w').innerText="Hi ,Peters"
+const heading=document.createElement('h2')
+heading.innerText="Tommorrow there is no training"
+heading.style.colour="green"
+document.body.style.background="blue"
+document.body.appendChild(heading)
+document.getElementsById('w').innerHTML="<h2>"+"By sunday need to revise core java,html,css and javascript</h2>"
+const bold=document.createElement('b')
+document.body.appendChild(form)
+const input=document.createElement(input)
+input.placeholder="Enter todo"
+form.appendChild(input)
+const Button=document.createElement(Button)
+button.inner="ADD"
+form.appendChild(Button)
